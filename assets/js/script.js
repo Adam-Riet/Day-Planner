da4708ba834ae04442dc318337febdf8dd4d3c3b
riet <adamriet@gmail.com>
2023-03-23 14:39:32 -0600

    reorganized js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var saveBtn = $('.saveBtn');
 
 
 //Function does not begin until DOM has loaded.
- $(document).ready(function() {
+    $(document).ready(function() {
     $('.saveBtn').on('click', function() {
        //Targeting parent of function which will match the the hour found in the first div.
     var parentDiv = $(this).parent();
@@ -18,81 +18,56 @@ var saveBtn = $('.saveBtn');
     
 //Variable to get current hour from dayjs to then compare it to hour in div. Code from class 5, activity 26.
 //change variable to make it an interger to better compare agaisnt variables created below.
-var time = parseInt(dayjs().format('H'));
+    var time = parseInt(dayjs().format('H'));
     //Loop to cycle through each element with time-block class. Extracts out the number in "hour-x" as an interger to compare it to var time's hour.
     $('.time-block').each(function() {
         //creating new variables to extract both the hour and AM/PM to compare to time variable.
-        var scheduleID = $(this).attr('id').split('-');
-        var scheduleHour = parseInt(scheduleID[1]);
-        var scheduleAmPm = scheduleID[2];
-
-        var parentDiv = $(this);
-        var textarea = parentDiv.find('textarea');
-        var parentDivID = parentDiv.attr('id');
+    var scheduleID = $(this).attr('id').split('-');
+        
+    var scheduleHour = parseInt(scheduleID[1]);
+        
+    var scheduleAmPm = scheduleID[2];
+        
+    var parentDiv = $(this);
+        
+    var textarea = parentDiv.find('textarea');
+        
+    var parentDivID = parentDiv.attr('id');
        
-        if (scheduleAmPm === "PM" && scheduleHour !== 12) {
-            scheduleHour += 12;
-        } else if (scheduleAmPm === "AM" && scheduleHour === 12) {
-            scheduleHour = 0;
-        }
+    if (scheduleAmPm === "PM" && scheduleHour !== 12) {
+        scheduleHour += 12;
+    } else if (scheduleAmPm === "AM" && scheduleHour === 12) {
+        scheduleHour = 0;
+    }
 
     //Applies correct color depending on current time. 
-        if (scheduleHour < time) {
-            $(this).addClass('past');
-        } else if (scheduleHour === time) {
-            $(this).addClass('present');
-        } else { 
-            $(this).addClass('future');
-        }
-
-       var parentDivID = $(this).attr("id");
-    var textarea = $(this).find("textarea");
-    var savedSchedule = localStorage.getItem(parentDivID);
-    if (savedSchedule !== null) {
-      textarea.val(savedSchedule);
+    if (scheduleHour < time) {
+        $(this).addClass('past');
+    } else if (scheduleHour === time) {
+        $(this).addClass('present');
+    } else { 
+        $(this).addClass('future');
     }
-    
-    
-    
+
+    var parentDivID = $(this).attr("id");
+        
+    var textarea = $(this).find("textarea");
+        
+    var savedSchedule = localStorage.getItem(parentDivID);
+        
+    if (savedSchedule !== null) {
+        textarea.val(savedSchedule);
+        }
     });
-
-    
-    //creating variable to save local store
-
-    
-
-
-    
-    
-    
-    
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     //Function utilizing dayjs to display current date and time. Thank you to UofU mini project for code. 
+    
     function displayTime() {
-        var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-        timeDisplayEl.text(rightNow);
-      }
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+    }
   
-      displayTime();
-      setInterval(displayTime, 1000);
+    displayTime();
+      
+    setInterval(displayTime, 1000);
   });
 
